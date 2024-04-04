bee62649621e748b8e153afd9e6f458e6598e88e
bobaeiiie <poidottee@gmail.com>
2024-04-05 03:19:19 +0900

    240405 query

---
+++ SQL/001_create_table.sql
@@ -0,0 +1,27 @@
+USE team1;
+
+CREATE TABLE informations (
+	NO				INT			PRIMARY KEY AUTO_INCREMENT
+	,NAME			VARCHAR(50)	NOT NULL
+	,age			DATE			NOT NULL
+	,gender		CHAR(1)		NOT NULL	COMMENT '0 : 남자, 1: 여자'
+	,created_at	DATETIME		NOT NULL	DEFAULT CURRENT_TIMESTAMP()
+	,deleted_at	DATETIME		NULL
+);
+
+CREATE TABLE todos (
+	list_no		INT			PRIMARY KEY AUTO_INCREMENT
+	,user_no		INT			NOT NULL
+	,todo_date	DATE			NOT NULL
+	,content		VARCHAR(50)	NOT NULL
+	,checked		CHAR(1)		NOT NULL	COMMENT '0 : 미완료, 1: 완료'
+	,created_at	DATETIME		NOT NULL	DEFAULT CURRENT_TIMESTAMP()
+	,updated_at	DATETIME		NOT NULL	DEFAULT CURRENT_TIMESTAMP()
+	,deleted_at	DATETIME		NULL 
+);
+
+ALTER TABLE todos ADD CONSTRAINT fk_todos_user_no FOREIGN KEY (user_no) REFERENCES informations(NO);
+
+
+ALTER TABLE informations CHANGE COLUMN age birth_at DATE NOT NULL;
+ALTER TABLE informations CHANGE COLUMN age birth_at DATE NOT NULL;
