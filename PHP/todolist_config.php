a810753a38a4db7e7b10f6ff221fd853efd28648
yoozeze <fbghrud3@naver.com>
2024-04-06 22:49:07 +0900

    20240405 catcare

--- PHP/todolist_config.php
+++ PHP/todolist_config.php
@@ -8,7 +8,10 @@ define("MARIADB_CHARSET", "utf8mb4");
 define("MARIADB_DSN", "mysql:host=".MARIADB_HOST.";dbname=".MARIADB_NAME.";charset=".MARIADB_CHARSET);
 
 define("ROOT", $_SERVER["DOCUMENT_ROOT"]."/");
-define("FILE_HEADER", ROOT."todolist_header.php");
+define("FILE_MYPAGE", ROOT."");
+define("FILE_JOIN", ROOT."");
+define("FILE_TODOLIST", ROOT."");
+define("FILE_CALENDAR", ROOT."");
 define("FILE_LIB_DB", ROOT."");
 
 
