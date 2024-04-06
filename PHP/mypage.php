91e3007e217cb4f38a851274bdbfb0ba89935714
bobaeiiie <poidottee@gmail.com>
2024-04-06 12:12:15 +0900

    240406 mypage php

---
+++ PHP/mypage.php
@@ -0,0 +1,70 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./CSS/mypage.css">
+</head>
+<body>
+    <header>
+        <div class="main-title">
+          <img src="/HTML/img/content-title.png" class="title-img">
+        </div>
+        <div class="header-profile-name">로미</div>
+        <a href="./mypage.html">
+          <img class="header-profile-img"
+                src="./img/11zon_cropped__1_-removebg-preview.png"/>
+        </a>
+      </header>
+    <main class="main-box">
+      <div class="box">
+        <div class="menu-content">
+            <div class="menu selected-menu">
+                <a href="./mypage.html">내정보</a>
+            </div>
+            <div class="menu">
+                <a href="./">알림</a>
+            </div>
+            <div class="menu">
+                <a href="./">캘린더</a>
+            </div>
+        </div>
+        <div class="content-info">
+            <div class="info-title">집사 반가워!</div>
+            <div class="info-box">
+                <div class="info-left">
+                    <img class="info-pic" src="./img/11zon_cropped__1_-removebg-preview.png" alt="">
+                    <a href=""><div class="info-edit">내 정보 수정</div></a> 
+                </div>
+                <div class="info-right">
+                    <span class="info-text1">로미</span>
+                    <span class="info-text2">남</span>
+                    <span class="info-text1">8개월</span>
+                    <span class="info-text2">4kg</span>
+                    <span class="dday">로미의 생일이 100일 남았습니다!</span>
+                    </div>
+                </div>
+                <div class="gage">
+                    <div class="gage-back">
+                        <span class="gage-text">70%</span>
+                        <div class="gage-bar">
+                            <span class="gage-text">행복달성지수</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+</html>
